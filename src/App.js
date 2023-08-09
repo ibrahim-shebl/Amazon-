3a42a5d736532644415ca3df0258f1fe26fb2450
ibrahim-shebl <hema01025946540@gmail.com>
2023-08-09 07:38:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+import { productsData } from "./api/api";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Signin from "./pages/Signin";
+import Registration from "./pages/Registration";
+const Layout =()=>{
+  return (
+    <div>
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} loader={productsData}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Route>
+        <Route path="/sigin" element={<Signin />}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
+      </Route>
+       
+    )
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-bodyFont bg-gray-100">
+<RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import logo from "./logo (1).png"
+import darkLogo from "./darkLogo.png";
+import bannerImgOne from "./banner/bannerImgTwo.jpg";
+import bannerImgTwo from "./banner/bannerImgOne.jpg";
+import bannerImgThree from "./banner/bannerImgThree.jpg";
+import bannerImgFour from "./banner/bannerImgFour.jpg";
+import bannerImgFive from "./banner/bannerImgFive.jpg";
+import egypt from "./egypt.png"
+import emptyCart from "./emptyCart.png";
+
+export {
+  logo,
+  darkLogo,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  bannerImgFour,
+  bannerImgFive,
+  emptyCart,
+  egypt,
+};
